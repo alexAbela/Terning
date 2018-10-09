1ed0b9a466c21cb30384e4a2720baa54942219b0
Martin <martingw@live.dk>
2018-10-09 10:07:27 +0200

    Bugfix af dice

---
+++ Dice.java
@@ -0,0 +1,17 @@
+package terning;
+
+public class Dice {
+    // roll the die and return the value (1-6)
+    public int roll() {
+        int die;
+        die = (int)(Math.random()*6) + 1;
+        return die;
+    }
+
+    // roll the die n times and print the values
+    public void rollMultiple(int n) {
+        for (int i=1; i<n; i++) {
+            System.out.print(roll() + " ");
+        }
+    }
+}
